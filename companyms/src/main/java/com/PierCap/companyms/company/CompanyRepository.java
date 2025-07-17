1ee082e9c6020ed42c1443dde22d3db4354ac024
PierangeloCap <118979192+PierangeloCap@users.noreply.github.com>
2025-07-17 09:17:28 +0200

    Add company microservice

---
+++ companyms/src/main/java/com/PierCap/companyms/company/CompanyRepository.java
@@ -0,0 +1,7 @@
+package com.PierCap.companyms.company;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Long>{
+    
+}
